8d8f227f435f93875052a39aaae3b7f2a477f928
cpeixin <421391491@qq.com>
2017-07-19 00:13:41 +0800

    读取parquet文件 filter算子操作

---
+++ spark2.0_Test/src/main/scala/Moudle/Spark2/program/UserBehaviorsAnalysis.scala
@@ -0,0 +1,44 @@
+package Moudle.Spark2.program
+
+import org.apache.log4j.{Level, Logger}
+import org.apache.spark.sql.SparkSession
+import org.slf4j.LoggerFactory
+
+/**
+  * Created by root on 2017/7/18.
+  * 分析用户行为
+  * 读取parquet文件
+  *
+  */
+object UserBehaviorsAnalysis {
+//  Logger.getLogger("debug").setLevel(Level.ERROR)
+  val logger = LoggerFactory.getLogger(this.getClass)
+
+  def main(args: Array[String]): Unit = {
+    val warehouseLocation = "/spark-warehouse"
+    val spark = SparkSession.builder()
+      .appName("userBehaviors")
+      .master("local[2]")
+      .config("spark.sql.warehouse.dir", warehouseLocation)
+      .getOrCreate()
+
+    import spark.implicits._
+    import org.apache.spark.sql.types._
+    import org.apache.spark.sql.functions._
+
+    val data = spark.read.format("parquet").parquet("data/users.parquet")
+//    data.show()
+//    +------+--------------+----------------+
+//    |  name|favorite_color|favorite_numbers|
+//    +------+--------------+----------------+
+//    |Alyssa|          null|  [3, 9, 15, 20]|
+//    |   Ben|           red|              []|
+//    +------+--------------+----------------+
+    //读取parquet文件后生成DF
+    //spark2.0中filter的写法
+    data.filter($"name".equalTo("Ben")).select("name","favorite_color").show()
+    //spark1.6中filter的写法
+    data.filter(data("name").equalTo("Ben")).select("name","favorite_color").show()
+
+  }
+}
