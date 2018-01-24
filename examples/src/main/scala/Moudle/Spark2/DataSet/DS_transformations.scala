1a1c1c5291708ea648b4449b6c463c0eb1981a24
cpeixin <421391491@qq.com>
2018-01-24 23:05:35 +0800

    normal

--- examples/src/main/scala/Moudle/Spark2/DataSet/DS_transformations.scala
+++ examples/src/main/scala/Moudle/Spark2/DataSet/DS_transformations.scala
@@ -10,13 +10,15 @@ object DS_transformations {
     val spark = SparkSession
       .builder()
       .appName("DatasetOps")
-      .master("local")
+      .master("local[2]")
       .config("spark.sql.warehouse.dir", "/spark-warehouse")
       .getOrCreate()
 
     val textFile = spark.read.textFile("examples/src/main/scala/Moudle/textfile")
     import spark.implicits._
-    val maxNum = textFile.map(line => line.split(" ").size).reduce((a, b) => if (a > b) a else b)
-    println(maxNum)
+//    val maxNum = textFile.map(line => line.split(" ").size).reduce((a, b) => if (a > b) a else b)
+//    val wordCount = textFile.flatMap(line => line.split(" ")).groupByKey(identity).count().collect()
+    val wordCount = textFile.rdd.flatMap(_.split(" ")).map((_, 1)).reduceByKey(_ + _)
+    println(wordCount.toDebugString)
   }
 }
